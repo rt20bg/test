f6d791824dca4bd41031ad5d6e697fefe08606df
rt20bg <51533101+rt20bg@users.noreply.github.com>
2022-11-02 00:05:23 +0200

    Add existing project files to Git

---
+++ Console App/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    class Class1
+    {
+//        Random rnd = new Random();
+//        List<Nobelov_Laureat> winners = new List<Nobelov_Laureat>();
+//            for (int i = 0; i< 954; i++)
+//            {
+//                Nobelov_Laureat item = new Nobelov_Laureat();
+//        item.day = rnd.Next(366);
+//                winners.Add(item);
+//            }
+
+
+
+//    List<Nobelov_Laureat> SortedWinners = winners.OrderBy(o => o.day).ToList();
+//    int longest = 1;
+//    int temp = 0;
+//            for (int i = 1; i<SortedWinners.Count; i++)
+//            {
+//                if (SortedWinners[i].day == SortedWinners[i - 1].day)
+//                {
+//                    temp++;
+//                }
+//                else
+//                {
+//                    if (longest<temp)
+//                    {
+//                        longest = temp;
+//                        temp = 0;
+//                    }
+//                }
+               
+//            }
+//            throw new Exception(""); //for manualy chekking the list :R
+//Console.WriteLine(longest);
+//            var x = Console.ReadKey();
+    }
+}
---
+++ Console App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[] x = new int[12];
+            for (int i = 0; i < 12; i++)
+            {
+                x[i] = 0;
+            }
+            for (int i = 0; i < 954; i++)
+            {
+                x[rnd.Next(12)]++;
+            }
+            Array.Sort(x);
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+        }
+    }
+}
